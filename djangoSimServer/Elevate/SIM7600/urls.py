3298516f4e4862146c8debd5e4cbe43ee079f419
kundan1911 <83800542+kundan1911@users.noreply.github.com>
2024-03-03 16:53:16 +0530

    Delete djangoSimServer directory

--- djangoSimServer/Elevate/SIM7600/urls.py
+++
@@ -1,9 +0,0 @@
-# sim7600/urls.py
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('make_call/', views.make_call, name='make_call'),
-    path('send_text_message/', views.send_text_message, name='send_text_message'),
-]
